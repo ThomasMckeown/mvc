982ef8c23815457e302ce7f587f5c519b16c31fa
ThomasMckeown <47249089+ThomasMckeown@users.noreply.github.com>
2019-03-01 15:08:08 +0000

    Added a navigation bar to make it easier to go from the index page to the gallery and back as well as to the character_list.php page

--- view/header.php
+++ view/header.php
@@ -9,4 +9,13 @@
 <!-- the body section -->
 <body>
 <header><h1>SMITE</h1></header>
+
+ <nav class="topNav">
+            <center>
+                <ul style="list-style-type: none">
+                    <li><i class="fas fa-ellipsis-v" style="color: #3090C7"></i>&nbsp;<a class="active" href="index.php?action=list_characters"><div id="geosansN">DATABASE</div></a>&nbsp;&nbsp;&nbsp;</li>
+                    <li><i class="fas fa-ellipsis-v" style="color: #3090C7"></i>&nbsp;<a  class="active" href="gallery.php"><div id="geosansN">GALLERY</div></a>&nbsp;&nbsp;&nbsp;</li>                    
+                </ul>
+            </center>
+        </nav>
        
