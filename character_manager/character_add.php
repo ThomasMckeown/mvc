be4cdcd061296ffaa39d64eaa7af0550cb2413cc
ThomasMckeown <47249089+ThomasMckeown@users.noreply.github.com>
2019-03-01 14:21:29 +0000

    deleted the image upload and moved it to a gallery.php page

--- character_manager/character_add.php
+++ character_manager/character_add.php
@@ -16,68 +16,24 @@ include "../model/database.php";
         <?php endforeach; ?>
         </select>
         <br>
-         <?php
-        if(isset($_FILES['image']))
-    {
-        echo 'running';
-        $image = $_FILES['image']['name'];
         
-       
-    
-        $target = "images/".basename($image);
-        if(!empty($image))
-        {
-            if(move_uploaded_file($_FILES['image']['tmp_name'], $target))
-            {
-
-                echo $target;
-               $query = "INSERT INTO character_image VALUES('".$target."')";
-               $statement = $db->prepare($query);
-               $statement->execute();
-               $statement->closeCursor();
-
-            }else
-            {
-
-                echo 'error';    
-
-            }
-        }else
-        {       
-            echo 'run';
-            createContentNoImage($topic_id,$type,$content,0,$author_id,getNextOrderNumber($topic_id));
-        }
-    }
-//    echo $id;
-        ?>
-        
-        <!--<form method="post" enctype="multipart/form-data">-->
-            
-        
-            <!--<input type="submit" name="insert" id="insert" value="insert"/>-->
-        <!--</form>-->
-        
-        
-        
-         <input type="file" name="image" id="image" accept="image/*"/>
-            <br>
-        
-        <label>Code:</label>
-        <input type="input" name="character_id">
+         <label>Character ID:</label>
+        <input type="input" name="character_id" required>
         <br>
 
         <label>Name:</label>
-        <input type="input" name="character_name">
+        <input type="input" name="character_name" required>
         <br>
 
-        <label>List Price:</label>
-        <input type="input" name="pantheon">
+        <label>Pantheon:</label>
+        <input type="input" name="pantheon" required>
+        <br>
         
-        <label>List Price:</label>
-        <input type="input" name="rank">
-        
-        <label>List Price:</label>
-        <input type="input" name="strong_crowd_control">
+        <label>Rank:</label>
+        <input type="input" name="rank" required>
+        <br>
+        <label>Strong Crowd Contol:</label>
+        <input type="input" name="strong_crowd_control" required pattern="[YNyn]">
         <br>
 
         <label>&nbsp;</label>
